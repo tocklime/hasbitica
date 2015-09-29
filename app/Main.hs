3b02afb41f797051a99143ce5734b04b7705a3d9
Greg Manning <git@app.tockli.me>
2015-09-29 19:55:48 +0100

    Now reads settings from ~/.habitica

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,33 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Hasbitica.Api
 import Control.Monad.Trans.Either
 import Servant.Common.Req
+import System.Directory (getHomeDirectory)
+import Data.Aeson (decode, FromJSON)
+import System.FilePath.Posix ((</>))
+import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as B
 
-apiKey :: String
-apiKey = "foo"
-apiUser :: String
-apiUser = "bar"
+data Settings = Settings {address,user,key::String}
+  deriving (Show,Generic)
 
-addTask :: String -> IO String
-addTask t = do
-  x <- runEitherT $ postTask (HabiticaApiKey apiUser apiKey) (todo t)
+instance FromJSON Settings
+
+readSettings :: IO (Maybe Settings)
+readSettings = fmap (</> ".habitica") getHomeDirectory >>= fmap decode . B.readFile
+
+addTask :: HabiticaApiKey -> String -> IO String
+addTask k t = do
+  x <- runEitherT $ postTask k (todo t)
   case x of
     Left err -> return (B.unpack $ responseBody err)
     Right _ -> return "OK"
 
 main :: IO ()
-main =  addTask "A HAHAHA" >>= putStrLn
+main = do
+  (Just s) <- readSettings 
+  let k = HabiticaApiKey (user s) (key s)
+  addTask k "A HAHAHA" >>= putStrLn
